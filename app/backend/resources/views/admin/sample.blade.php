1036e59fe1bceee49bb7c577cf5f2a2189d06887
Fumihiro_Maejima <testerwatson1101@gmail.com>
2024-04-16 23:57:00 +0900

    add Select2.

--- app/backend/resources/views/admin/sample.blade.php
+++ app/backend/resources/views/admin/sample.blade.php
@@ -460,6 +460,58 @@
     @endpush
 
 
+    {{-- Select2 --}}
+    {{-- Minimal --}}
+    <x-adminlte-select2 name="sel2Basic">
+        <option>Option 1</option>
+        <option disabled>Option 2</option>
+        <option selected>Option 3</option>
+    </x-adminlte-select2>
+
+    {{-- Disabled --}}
+    <x-adminlte-select2 name="sel2Disabled" disabled>
+        <option>Option 1</option>
+        <option>Option 2</option>
+    </x-adminlte-select2>
+
+    {{-- With prepend slot, label, and data-placeholder config --}}
+    <x-adminlte-select2 name="sel2Vehicle" label="Vehicle" label-class="text-lightblue"
+        igroup-size="lg" data-placeholder="Select an option...">
+        <x-slot name="prependSlot">
+            <div class="input-group-text bg-gradient-info">
+                <i class="fas fa-car-side"></i>
+            </div>
+        </x-slot>
+        <option/>
+        <option>Vehicle 1</option>
+        <option>Vehicle 2</option>
+    </x-adminlte-select2>
+
+    {{-- With multiple slots, and plugin config parameters --}}
+    @php
+        $config = [
+            "placeholder" => "Select multiple options...",
+            "allowClear" => true,
+        ];
+    @endphp
+    <x-adminlte-select2 id="sel2Category" name="sel2Category[]" label="Categories"
+        label-class="text-danger" igroup-size="sm" :config="$config" multiple>
+        <x-slot name="prependSlot">
+            <div class="input-group-text bg-gradient-red">
+                <i class="fas fa-tag"></i>
+            </div>
+        </x-slot>
+        <x-slot name="appendSlot">
+            <x-adminlte-button theme="outline-dark" label="Clear" icon="fas fa-lg fa-ban text-danger"/>
+        </x-slot>
+        <option>Sports</option>
+        <option>News</option>
+        <option>Games</option>
+        <option>Science</option>
+        <option>Maths</option>
+    </x-adminlte-select2>
+
+
     <div class="container">
         <div class="row">
             <div class="col-sm">
