ec5cd6ff24c8f649e691a2dcf1f5cf5870edb096
Fumihiro_Maejima <testerwatson1101@gmail.com>
2024-04-20 23:07:23 +0900

    remove unused.

--- app/backend/resources/views/admin/sample2.blade.php
+++ app/backend/resources/views/admin/sample2.blade.php
@@ -9,45 +9,8 @@
 @section('content')
     <p>Welcome to this beautiful test admin panel.</p>
 
-
     <p>SampleComponents.</p>
 
-
-    {{-- Alert --}}
-
-    {{-- Minimal with title and dismissable --}}
-    <x-adminlte-alert title="Well done!" dismissable>
-        Minimal example
-    </x-adminlte-alert>
-
-    {{-- Minimal with icon only --}}
-    <x-adminlte-alert icon="fas fa-user">
-        User has logged in!
-    </x-adminlte-alert>
-
-    <x-adminlte-alert theme="dark" title="Important">
-        Dark theme alert!
-    </x-adminlte-alert>
-
-    <div class="container">
-        <div class="row">
-            <div class="col-sm">
-                <x-adminlte-button label="Button"/>
-                <x-adminlte-button label="Disabled" theme="dark" disabled/>
-
-                {{-- Button with themes and icons --}}
-                <x-adminlte-button label="Primary" theme="primary" icon="fas fa-key"/>
-                <x-adminlte-button label="Secondary" theme="secondary" icon="fas fa-hashtag"/>
-                <x-adminlte-button label="Info" theme="info" icon="fas fa-info-circle"/>
-                <x-adminlte-button label="Warning" theme="warning" icon="fas fa-exclamation-triangle"/>
-                <x-adminlte-button label="Danger" theme="danger" icon="fas fa-ban"/>
-                <x-adminlte-button label="Success" theme="success" icon="fas fa-thumbs-up"/>
-                <x-adminlte-button label="Dark" theme="dark" icon="fas fa-adjust"/>
-        </div>
-    </div>
-
-
-    {{-- With label, invalid feedback disabled, and form group class --}}
     <div class="container">
         <div class="row">
             <div class="col-sm">
@@ -57,140 +20,31 @@
 
                 <x-adminlte-input name="iAddress" label="Address" placeholder="address" fgroup-class="col-md-6" disable-feedback/>
                 <x-adminlte-input name="iPassword" label="Password" type="password" placeholder="password" fgroup-class="col-md-6" disable-feedback/>
-                <x-adminlte-input-file name="ifMin" label="upload file"/>
-                <x-adminlte-input-date name="idBasic" placeholder="input date"/>
-                @php
-                $config = [
-                    'format' => 'YYYY-MM-DD HH.mm',
-                    'dayViewHeaderFormat' => 'MMM YYYY',
-                    'minDate' => "js:moment().startOf('month')",
-                    'maxDate' => "js:moment().endOf('month')",
-                    'daysOfWeekDisabled' => [0, 6],
-                ];
-                @endphp
-                <x-adminlte-input-date name="idSizeSm" label="Working Datetime" igroup-size="sm"
-                    :config="$config" placeholder="Choose a working day...">
-                    <x-slot name="appendSlot">
-                        <div class="input-group-text bg-dark">
-                            <i class="fas fa-calendar-day"></i>
-                        </div>
-                    </x-slot>
-                </x-adminlte-input-date>
+                <x-adminlte-input-file name="ifMin" label="upload file" fgroup-class="col-md-6"/>
+                <x-adminlte-input-date name="idBasic" placeholder="input date" label="instaDate" fgroup-class="col-md-6"/>
+                <x-adminlte-select2 name="sel2Basic" label="testSelect" fgroup-class="col-md-6">
+                    <option>Option 1</option>
+                    <option disabled>Option 2</option>
+                    <option selected>Option 3</option>
+                </x-adminlte-select2>
+                <x-adminlte-textarea name="taBasic" label="testTextArea" placeholder="Insert description..." fgroup-class="col-md-6"/>
             </div>
             </div>
         </div>
     </div>
 
-    {{-- With prepend slot --}}
-    <x-adminlte-input name="iUser" label="User" placeholder="username" label-class="text-lightblue">
-        <x-slot name="prependSlot">
-            <div class="input-group-text">
-                <i class="fas fa-user text-lightblue"></i>
+    <div class="container">
+        <div class="row">
+            <div class="col-sm">
+                <x-adminlte-button label="Primary" theme="primary" icon="fas fa-key"/>
+                <x-adminlte-button label="Secondary" theme="secondary" icon="fas fa-hashtag"/>
+                <x-adminlte-button label="Info" theme="info" icon="fas fa-info-circle"/>
+                <x-adminlte-button label="Warning" theme="warning" icon="fas fa-exclamation-triangle"/>
+                <x-adminlte-button label="Danger" theme="danger" icon="fas fa-ban"/>
+                <x-adminlte-button label="Success" theme="success" icon="fas fa-thumbs-up"/>
             </div>
-        </x-slot>
-    </x-adminlte-input>
-
-    {{-- Input File --}}
-
-    {{-- Minimal --}}
-    <x-adminlte-input-file name="ifMin"/>
-
-    {{-- With label and feedback disabled --}}
-    <x-adminlte-input-file name="ifLabel" label="Upload file" placeholder="Choose a file..." disable-feedback/>
-
-    {{-- With multiple slots and multiple files --}}
-    {{--  <x-adminlte-input-file
-        id="ifMultiple"
-        name="ifMultiple[]"
-        label="Upload files"
-        placeholder="Choose multiple files..."
-        igroup-size="lg"
-        legend="Choose"
-        multiple
-    >
-        <x-slot name="appendSlot">
-            <x-adminlte-button theme="primary" label="Upload"/>
-        </x-slot>
-        <x-slot name="prependSlot">
-            <div class="input-group-text text-primary">
-                <i class="fas fa-file-upload"></i>
-            </div>
-        </x-slot>
-    </x-adminlte-input-file>  --}}
-
-
-    {{-- Select2 --}}
-    {{-- Minimal --}}
-    <x-adminlte-select2 name="sel2Basic">
-        <option>Option 1</option>
-        <option disabled>Option 2</option>
-        <option selected>Option 3</option>
-    </x-adminlte-select2>
-
-    {{-- With prepend slot, label, and data-placeholder config --}}
-    <x-adminlte-select2 name="sel2Vehicle" label="Vehicle" label-class="text-lightblue" igroup-size="lg" data-placeholder="Select an option... Select2">
-        <x-slot name="prependSlot">
-            <div class="input-group-text bg-gradient-info">
-                <i class="fas fa-car-side"></i>
-            </div>
-        </x-slot>
-        <option/>
-        <option>Vehicle 1</option>
-        <option>Vehicle 2</option>
-    </x-adminlte-select2>
-
-    {{-- With multiple slots, and plugin config parameters --}}
-    @php
-        $config = [
-            "placeholder" => "Select multiple options...",
-            "allowClear" => true,
-        ];
-    @endphp
-    <x-adminlte-select2 id="sel2Category" name="sel2Category[]" label="Categories"
-        label-class="text-danger" igroup-size="sm" :config="$config" multiple>
-        <x-slot name="prependSlot">
-            <div class="input-group-text bg-gradient-red">
-                <i class="fas fa-tag"></i>
-            </div>
-        </x-slot>
-        <x-slot name="appendSlot">
-            <x-adminlte-button theme="outline-dark" label="Clear" icon="fas fa-lg fa-ban text-danger"/>
-        </x-slot>
-        <option>Sports</option>
-        <option>News</option>
-        <option>Games</option>
-        <option>Science</option>
-        <option>Maths</option>
-    </x-adminlte-select2>
-
-    {{-- TextArea --}}
-    {{-- Minimal with placeholder --}}
-    <x-adminlte-textarea name="taBasic" placeholder="Insert description..."/>
-
-    {{-- With prepend slot, sm size, and label --}}
-    <x-adminlte-textarea name="taDesc" label="Description" rows=5 label-class="text-warning"
-        igroup-size="sm" placeholder="Insert description...">
-        <x-slot name="prependSlot">
-            <div class="input-group-text bg-dark">
-                <i class="fas fa-lg fa-file-alt text-warning"></i>
-            </div>
-        </x-slot>
-    </x-adminlte-textarea>
-
-    {{-- With slots, sm size, and feedback disabled --}}
-    <x-adminlte-textarea name="taMsg" label="Message" rows=5 igroup-size="sm"
-        label-class="text-primary" placeholder="Write your message..." disable-feedback>
-        <x-slot name="prependSlot">
-            <div class="input-group-text">
-                <i class="fas fa-lg fa-comment-dots text-primary"></i>
-            </div>
-        </x-slot>
-        <x-slot name="appendSlot">
-            <x-adminlte-button theme="primary" icon="fas fa-paper-plane" label="Send"/>
-        </x-slot>
-    </x-adminlte-textarea>
-
-
+        </div>
+    </div>
 
     {{-- DataTable --}}
     {{-- Setup data for datatables --}}
