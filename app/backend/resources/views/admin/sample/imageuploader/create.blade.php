743b802e6aaa87dfc775472aaef038ed0b0049cc
Fumihiro_Maejima <testerwatson1101@gmail.com>
2024-05-01 01:46:06 +0900

    add innput datetime setting.

--- app/backend/resources/views/admin/sample/imageuploader/create.blade.php
+++ app/backend/resources/views/admin/sample/imageuploader/create.blade.php
@@ -18,15 +18,25 @@
                     @csrf
                     <x-adminlte-input name="name" label="name" placeholder="name" fgroup-class="col-md-6" value={{$name}}/>
                     <x-adminlte-select name="testSelet1" label="testSelect1" fgroup-class="col-md-6">
-                        <x-adminlte-options :options="['Option 1', 'Option 2', 'Option 3']" disabled="1"
+                        <x-adminlte-options :options="[1 => 'Option 1', 2 => 'Option 2', 3 => 'Option 3']" disabled="1"
                             empty-option="Select an option..."/>
                     </x-adminlte-select>
                     <div class="form-group col-md-6">
                         <label for="testDate">inputDate</label>
-                        <div class="input-group">
-                            <input name="testDate" type="date" placeholder="input date" label="testDate" class="form-control"/>
-                            <input name="testTime" type="time" placeholder="HH:mm" label="testTime" class="form-control"/>
+                        <div class="input-group @error('testDate') adminlte-invalid-igroup @enderror  @error('testTime') adminlte-invalid-igroup @enderror">
+                            <input name="testDate" type="date" placeholder="input date" label="testDate" class="form-control @error('testDate') is-invalid @enderror"/>
+                            <input name="testTime" type="time" placeholder="HH:mm" label="testTime" class="form-control @error('testTime') is-invalid @enderror"/>
                         </div>
+                        @error('testDate')
+                            <span class="invalid-feedback d-block" role="alert">
+                                <strong>{{$message}}</strong>
+                            </span>
+                        @enderror
+                        @error('testTime')
+                            <span class="invalid-feedback d-block" role="alert">
+                                <strong>{{$message}}</strong>
+                            </span>
+                        @enderror
                     </div>
                     <x-adminlte-input-file name="file" label="upload file" fgroup-class="col-md-6" value={{$image}}/>
                     <x-adminlte-select2 name="testSelet2" label="testSelect2" fgroup-class="col-md-6">
