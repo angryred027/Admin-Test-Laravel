8d4f697e3804518c63b3a505d9cc623b9c9dd261
Fumihiro_Maejima <testerwatson1101@gmail.com>
2024-05-25 22:32:50 +0900

    add radio buttion.

---
+++ app/backend/resources/views/components/form/sample-radio-button.blade.php
@@ -0,0 +1,14 @@
+<div class="form-group clearfix">
+    <div class="icheck-primary d-inline">
+        <input type="radio" id="radioPrimary1" name="r1" checked="">
+        <label for="radioPrimary1"></label>
+    </div>
+    <div class="icheck-primary d-inline">
+        <input type="radio" id="radioPrimary2" name="r1">
+        <label for="radioPrimary2"></label>
+    </div>
+    <div class="icheck-primary d-inline">
+        <input type="radio" id="radioPrimary3" name="r1">
+        <label for="radioPrimary3">Primary radio</label>
+    </div>
+</div>
