636abf65a0369ee73d3c3b4ff6c605fafa9f9861
Fumihiro_Maejima <testerwatson1101@gmail.com>
2024-05-25 16:19:54 +0900

    add maxlength design.

--- app/backend/resources/views/components/form/sample-text-area.blade.php
+++ app/backend/resources/views/components/form/sample-text-area.blade.php
@@ -6,7 +6,7 @@
     'value' => null,
     'required' => false,
     'disabled' => false,
-    'maxLength' => 1000,
+    'maxlength' => 1000,
     'cols' => 20,
     'rows' => 2,
 ])
@@ -20,7 +20,7 @@
                     name="{{$name}}"
                     placeholder="{{$placeholder}}"
                     label="{{$name}}"
-                    maxlength="{{$maxLength}}"
+                    maxlength="{{$maxlength}}"
                     cols="{{$cols}}"
                     rows="{{$rows}}"
                     {{ $required ? 'required' : '' }}
@@ -29,7 +29,7 @@
                 >{{$value ?? ''}}</textarea>
             </div>
             <div class="d-flex justify-content-end text-secondary text-sm">
-                <span id="{{$name . '_text_counter'}}">{{mb_strlen($value ?? '')}}</span>/{{$maxLength}}
+                <span id="{{$name . '_text_counter'}}" class="text_area_test_counter">{{mb_strlen($value ?? '')}}</span>/{{$maxlength}}
             </div>
         </div>
     </div>
@@ -40,18 +40,27 @@
     @enderror
 </div>
 
+@section('css')
+    @parent
+    <style>
+        .text_area_test_counter.text_area_test_counter_is_max_length {
+            color: #dc3545;
+        }
+    </style>
+@stop
 
 @section('js')
     @parent
     <script>
-        initTextAreaComponent(`{{$name}}`);
+        initTextAreaComponent(`{{$name}}`, `{{$maxlength}}`);
 
         /**
         * initialize
         * @param {string} name
+        * @param {number} maxlength
         * @return {void}
         */
-        function initTextAreaComponent(name) {
+        function initTextAreaComponent(name, maxlength) {
             const textaAreaId = name + '_text_area'
             const textCounterId = name + '_text_counter'
             const textArea = document.getElementById(textaAreaId);
@@ -59,6 +68,12 @@
 
             if (textArea && textCounter) {
                 textArea.addEventListener('keyup', function(event){
+                    if (textArea.value.length >= maxlength) {
+                        textCounter.classList.add('text_area_test_counter_is_max_length')
+                    } else if (parseInt(textCounter.textContent) >= maxlength) {
+                        // 現在値が最大値の場合
+                        textCounter.classList.remove('text_area_test_counter_is_max_length')
+                    }
                     textCounter.textContent = textArea.value.length
                 });
             }
