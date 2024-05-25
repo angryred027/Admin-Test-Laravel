07682b4318f7dea148971fd99439d54b393ddf65
Fumihiro_Maejima <testerwatson1101@gmail.com>
2024-05-25 22:48:09 +0900

    remove d-inline.

--- app/backend/resources/views/components/form/sample-checkbox.blade.php
+++ app/backend/resources/views/components/form/sample-checkbox.blade.php
@@ -1,14 +1,14 @@
-<div class="form-group clearfix">
+<div class="form-group col-md-6">
     <div class="d-flex flex-column">
-        <div class="icheck-danger d-inline">
+        <div class="icheck-danger">
             <input type="checkbox" checked="" id="checkboxDanger1">
             <label for="checkboxDanger1">checkbox label1</label>
         </div>
-        <div class="icheck-danger d-inline">
+        <div class="icheck-danger">
             <input type="checkbox" id="checkboxDanger2">
             <label for="checkboxDanger2">checkbox label2</label>
         </div>
-        <div class="icheck-danger d-inline">
+        <div class="icheck-danger">
             <input type="checkbox" disabled="" id="checkboxDanger3">
             <label for="checkboxDanger3">checkbox label3</label>
         </div>
--- app/backend/resources/views/components/form/sample-radio-button.blade.php
+++ app/backend/resources/views/components/form/sample-radio-button.blade.php
@@ -1,14 +1,14 @@
-<div class="form-group clearfix">
+<div class="form-group clearfix col-md-6">
     <div class="d-flex flex-column">
-        <div class="icheck-primary d-inline">
+        <div class="icheck-primary">
             <input type="radio" id="radioPrimary1" name="r1" checked="">
             <label for="radioPrimary1">radio label1</label>
         </div>
-        <div class="icheck-primary d-inline">
+        <div class="icheck-primarye">
             <input type="radio" id="radioPrimary2" name="r1">
             <label for="radioPrimary2">radio label2</label>
         </div>
-        <div class="icheck-primary d-inline">
+        <div class="icheck-primary">
             <input type="radio" id="radioPrimary3" name="r1">
             <label for="radioPrimary3">radio label3</label>
         </div>
