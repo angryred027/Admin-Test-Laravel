f9a32f1410ea9a1afba23684dee35e75b323545e
Fumihiro_Maejima <testerwatson1101@gmail.com>
2024-05-27 01:35:04 +0900

    add [] to name.

--- app/backend/resources/views/components/form/sample-checkbox.blade.php
+++ app/backend/resources/views/components/form/sample-checkbox.blade.php
@@ -7,15 +7,15 @@
 <div class="form-group col-md-6">
     <div class="d-flex flex-column">
         <div class="icheck-danger">
-            <input type="checkbox" checked="" id="checkboxDanger1" name={{$name}} value="1">
+            <input type="checkbox" checked="" id="checkboxDanger1" name={{$name . '[]'}} value="1">
             <label for="checkboxDanger1">checkbox label1</label>
         </div>
         <div class="icheck-danger">
-            <input type="checkbox" id="checkboxDanger2" name={{$name}} value="2">
+            <input type="checkbox" id="checkboxDanger2" name={{$name . '[]'}} value="2">
             <label for="checkboxDanger2">checkbox label2</label>
         </div>
         <div class="icheck-danger">
-            <input type="checkbox" disabled="" id="checkboxDanger3" name={{$name}} value="3">
+            <input type="checkbox" disabled="" id="checkboxDanger3" name={{$name . '[]'}} value="3">
             <label for="checkboxDanger3">checkbox label3</label>
         </div>
     </div>
