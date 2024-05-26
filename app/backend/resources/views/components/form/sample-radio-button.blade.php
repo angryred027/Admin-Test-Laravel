941854ebf7841d05668db29383079fe6fef5fbb6
Fumihiro_Maejima <testerwatson1101@gmail.com>
2024-05-26 23:44:13 +0900

    add anem,value setting.

--- app/backend/resources/views/components/form/sample-radio-button.blade.php
+++ app/backend/resources/views/components/form/sample-radio-button.blade.php
@@ -1,15 +1,21 @@
+@props([
+    'class' => '',
+    'name' => '',
+    'value' => null,
+    'required' => false,
+])
 <div class="form-group clearfix col-md-6">
     <div class="d-flex flex-column">
         <div class="icheck-primary">
-            <input type="radio" id="radioPrimary1" name="r1" checked="">
+            <input type="radio" id="radioPrimary1" name={{$name}} checked="" value="1">
             <label for="radioPrimary1">radio label1</label>
         </div>
         <div class="icheck-primarye">
-            <input type="radio" id="radioPrimary2" name="r1">
+            <input type="radio" id="radioPrimary2" name={{$name}} value="2">
             <label for="radioPrimary2">radio label2</label>
         </div>
         <div class="icheck-primary">
-            <input type="radio" id="radioPrimary3" name="r1">
+            <input type="radio" id="radioPrimary3" name={{$name}} value="3">
             <label for="radioPrimary3">radio label3</label>
         </div>
     </div>
