f0d46532a2e6866801918eb68a8b493c7d1bb508
Fumihiro_Maejima <testerwatson1101@gmail.com>
2024-06-09 00:16:51 +0900

    add layout.blad.

---
+++ app/backend/resources/views/admin/layout.blade.php
@@ -0,0 +1,26 @@
+@extends('adminlte::page')
+
+@section('title', 'Base Dashboard')
+
+@section('content_header')
+    <h1>Base Dashboard</h1>
+@stop
+
+@section('content')
+    <p>Welcome to this beautiful base admin panel.</p>
+@stop
+
+@section('adminlte_css')
+    @parent
+    <link rel="stylesheet" href="/css/admin_custom.css">
+    {{--  動的に設定するCSS  --}}
+    <style>
+        .brand-link-color {
+            background-color: {{ config('myapp.mainColor') }};
+        }
+    </style>
+@stop
+
+@section('js')
+    <script> console.log('Test!'); </script>
+@stop
